68f1755c82638bce11226387a46ed628307373af
Micael Souza <micael.ns@hotmail.com>
2025-06-17 23:51:29 -0300

    Ajustes mensagens mais curtas (conta) + melhoria mensagem erro, removido pacote errors

--- conta/conta.go
+++ conta/conta.go
@@ -1,7 +1,6 @@
 package conta
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -17,21 +16,30 @@ type Conta struct {
 // Depositar adiciona valor ao saldo da conta.
 func (c *Conta) Depositar(valor float64) {
 	c.Saldo += valor
-	fmt.Printf("Depósito de R$%.2f realizado. Saldo atual: R$%.2f\n", valor, c.Saldo)
+	fmt.Printf("[Conta %s] + R$%.2f\n", c.Numero, valor)
 }
 
 // Sacar tenta subtrair valor do saldo.
 func (c *Conta) Sacar(valor float64) error {
 	if valor > c.Saldo {
-		return errors.New("saldo insuficiente")
+		return fmt.Errorf("[Conta %s] - R$%.2f (saldo insuficiente)",c.Numero, valor)
 	}
 	c.Saldo -= valor
-	fmt.Printf("Saque de R$%.2f realizado. Saldo atual: R$%.2f\n", valor, c.Saldo)
+	fmt.Printf("[Conta %s] - R$%.2f\n", c.Numero,  valor)
 	return nil
 }
 
+// Imprime saldo.
+func (c *Conta) ExtratoSimples() {
+	fmt.Printf("\n--- Saldo ---\n")
+	fmt.Printf("Agência: %s | Conta: %s\n", c.Agencia, c.Numero)
+	fmt.Printf("Saldo atual: R$%.2f\n", c.Saldo)
+	fmt.Println("---------------")
+	fmt.Println()
+}
+
 // Extrato imprime os dados da conta e saldo.
-func (c *Conta) Extrato() {
+func (c *Conta) ExtratoCompleto() {
 	fmt.Printf("\n--- Extrato ---\n")
 	fmt.Printf("Cliente: %s\n", c.Cliente)
 	fmt.Printf("Agência: %s | Conta: %s\n", c.Agencia, c.Numero)
